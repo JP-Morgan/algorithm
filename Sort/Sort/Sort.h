7203c5a2a5f3cb554197cc849179e9502ee7e66e
morgan5250 <2509071851@qq.com>
2023-04-05 17:48:50 +0800

    the first time

---
+++ Sort/Sort/Sort.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+//算法测试接口
+void TestOP();
+Print(int* a, int n);
+// 插入排序
+void InsertSort(int* a,int n);
+
+
+// 希尔排序
+// 1.预排序目的：数组接近有序--->分组插排
+// 分组排序：间隔为GAP分为
+// 2.直接插入排序
+//
+void ShellSort(int* a,int n);
+//
+
+//
+
+//
+
+//
---
+++ Sort/Sort/test.c
@@ -0,0 +1,16 @@
+#include "Sort.h"
+
+// 测试排序的性能对比
+void test()
+{
+	int a[] = { 5,3,7,8,10,2,4,1,6,9 };
+	Print(a, sizeof(a) / sizeof(a[0]));
+	InsertSort(a, sizeof(a) / sizeof(a[0]));
+	Print(a, sizeof(a) / sizeof(a[0]));
+}
+int main()
+{
+	TestOP();
+	//test();
+	return 0;
+}
